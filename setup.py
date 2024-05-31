199a538ad75b729b36b20fafd786ded0a5c02ead
Julian <julian@schibberges.de>
2024-05-31 08:10:16 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     readme="README.md",
     license="MIT",
     name='bundestag_api',
-    version='1.0.5',
+    version='1.0.6',
     packages=find_packages(),
     install_requires=[
          'requests>=2.0.0',

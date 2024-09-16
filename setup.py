2c4f0d3f5f64b1dcfdc6d0031087ce2ae84c33e0
Julian <julian@schibberges.de>
2024-09-16 16:59:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     readme="README.md",
     license="MIT",
     name='bundestag_api',
-    version='1.0.7',
+    version='1.0.8',
     packages=find_packages(),
     install_requires=[
          'requests>=2.0.0',
